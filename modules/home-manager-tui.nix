b86525a6a8fa49c36ef26a80c10a0ead70e26324
Alexey Kislin <alexey.kislin@gmail.com>
2025-09-14 08:19:19 +0000

    Git aliases

--- modules/home-manager-tui.nix
+++ modules/home-manager-tui.nix
@@ -14,6 +14,14 @@
       home.packages = [ ];
       programs.fish.enable = true;
 
+      programs.git = {
+        enable = true;
+
+        aliases = {
+          lg = '' log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all'';
+        };
+      };
+
       home.stateVersion = "25.05";
     };
 }
