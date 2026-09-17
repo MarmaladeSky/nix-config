8305daee55841357f10eba4fd136c3b082f1f7a4
Alexey Kislin <alexey.kislin@gmail.com>
2026-09-17 10:55:05 -0700

    Simple update

--- modules/home-manager-tui.nix
+++ modules/home-manager-tui.nix
@@ -318,7 +318,7 @@
       programs.git = {
         enable = true;
 
-        aliases = {
+        settings.alias = {
           lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
         };
       };
